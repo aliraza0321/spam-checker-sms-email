1972528d2d8355e795c12fc106592795e8325a5f
Ali Raza <alirazamehar0321@gmail.com>
2026-05-18 06:16:46 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
 import streamlit as st
 import pickle
-
-import os
-
-# correct path handling
-BASE_DIR = os.path.dirname(__file__)
-
-model = pickle.load(open(os.path.join(BASE_DIR, "model.pkl"), "rb"))
-vectorizer = pickle.load(open(os.path.join(BASE_DIR, "vectorizer.pkl"), "rb"))
-
-
+import string
+#load model in read only mode 
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+#text cleaning on user data
+def clean_text(text):
+    # convert lowercase
+    text = text.lower()
+    # remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # remove extra spaces
+    text = " ".join(text.split())
+    return text
+#title of our user interface
 st.title("SMS & Email Spam Detector")
-
-option = st.selectbox("Select Input Type", ["SMS", "Email"])
-
+#user selection box
+option = st.selectbox(
+    "Select Input Type",
+    ["SMS", "Email"]
+)
+#if option is sms
 if option == "SMS":
-    message = st.text_area("Enter SMS")
-
-    if st.button("Check"):
-        vec = vectorizer.transform([message])
-        result = model.predict(vec)[0]
-
-        if result == 1:
-            st.error("Spam !")
+    message = st.text_area("Enter SMS")#get input
+    if st.button("Check SMS"):
+        # if user did not enter any thing 
+        if message.strip() == "":
+            st.warning("Please enter an SMS first!")
         else:
-            st.success("Ham (not spam) ")
+            # cleaninng on data
+            clean_message = clean_text(message)
+            #convert in numerical 
+            vec = vectorizer.transform([clean_message])
+            # predict result
+            result = model.predict(vec)[0]
+            # output
+            if result == 1:
+                st.error("Spam Message Detected!")
+            else:
+                st.success("Ham Message (Not Spam)")
 
+#email checking
 else:
     subject = st.text_input("Email Subject")
     body = st.text_area("Email Body")
-
-    if st.button("Check"):
-        full_text = subject + " " + body
-        vec = vectorizer.transform([full_text])
-        result = model.predict(vec)[0]
-
-        if result == 1:
-            st.error("Spam !")
+    if st.button("Check Email"):
+        # input validation on user input
+        if subject.strip() == "" or body.strip() == "":
+            st.warning("Please enter email completely!")
         else:
-            st.success("Ham (not spam )")
+            # combine subject and body 
+            full_text = subject + " " + body
+            # cleaning data
+            clean_email = clean_text(full_text)
+            # conversion
+            vec = vectorizer.transform([clean_email])
+            # prediction
+            result = model.predict(vec)[0]
+            # output
+            if result == 1:
+                st.error("Spam Email Detected!")
+            else:
+                st.success("Ham Email (Not Spam)")
